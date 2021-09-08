b148dd2935e9ea1db9262e3e9e8398abb6c1fbd9
Rizwan Khan <rizwi23.rk@gmail.com>
2021-09-08 19:17:34 +0530

    updated cards

--- src/app/components/landingPage/index.tsx
+++ src/app/components/landingPage/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import data from "../../utils/movies.json";
 import { IMovie } from "../../models/movie";
 import { getShadows } from "../../styles/commonStyles";
-import { getTheme } from "@fluentui/react";
+import { getTheme, Rating, RatingSize, Text } from "@fluentui/react";
 import "./index.scss";
 
 export class LandingPage extends React.Component {
@@ -13,6 +13,12 @@ export class LandingPage extends React.Component {
 
     this.movieData = data.movies;
   }
+
+  getListingType = (listingType: string): string => {
+    if (listingType === "NOW_SHOWING") return "Now Showing";
+    return "Upcoming";
+  };
+
   render(): JSX.Element {
     const theme = getTheme();
 
@@ -22,10 +28,17 @@ export class LandingPage extends React.Component {
           <div className="ms-Grid-row">
             {this.movieData.map((movieData) => {
               return (
-                <div className="ms-Grid-col ms-sm12 ms-md6 ms-lg4">
+                <div className="ms-Grid-col ms-sm12 ms-lg6 ms-xl4">
                   <div className={`card ${getShadows(theme)}`}>
                     <img alt="poster" src={movieData.Poster} width="100%" />
-                    {movieData.Title}
+                    <Text>{this.getListingType(movieData.listingType)}</Text>
+                    <div className="movieTitle"><h3>{movieData.Title}</h3></div>
+                    <Rating
+                      max={10}
+                      size={RatingSize.Small}
+                      defaultRating={parseFloat(movieData.imdbRating)}
+                      readOnly
+                    />
                   </div>
                 </div>
               );
